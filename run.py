eaafe0fe4fc08bdee381ab91fc475faaa0e192a3
FedeC <24813315+turbostar190@users.noreply.github.com>
2024-07-01 16:13:25 +0200

    run script fixes

--- run.py
+++ run.py
@@ -2,9 +2,20 @@
 
 import os
 import time
+import sys
 
-random_numbers = [10, 20, 30]
-simulations = [2, 5, 10]#, 20, 30, 50]
+random_numbers = [5, 10, 15]
+simulations = [2, 5, 10, 20, 30, 50]
+
+# get parameters from command line
+if len(sys.argv) != 2:
+  print("Usage: python3 run.py <test_name>")
+  sys.exit(1)
+test_name = sys.argv[1]
+if test_name not in ['burst', 'scatter']:
+  print("Invalid test name. Use 'burst' or 'scatter'")
+  sys.exit(1)
+
 
 # get now datetime as YYMMDD_HHMMSS
 now = time.strftime('%y%m%d_%H%M%S')
@@ -28,7 +39,7 @@ for sim in simulations:
     # run
     os.system(f'cooja_nogui nd-test-mrm-{sim}n.csc')
 
-    results_dir = f'results/test_{sim}_s{n}'
+    results_dir = f'results/{test_name}_{sim}_s{n}'
 
     # mkdir if not exists
     os.system(f'mkdir -p {results_dir}')
@@ -38,5 +49,5 @@ for sim in simulations:
     os.system(f'mv test_dc.log {results_dir}')
 
     # run python script and save the output in a file, append if exists
-    os.system(f'python3 discovery.py {results_dir}/test.log >> results/{now}_dr.txt')
-    os.system(f'python3 energest-stats.py {results_dir}/test.log >> results/{now}_dc.txt')
+    os.system(f'python3 discovery.py {results_dir}/test.log >> results/{test_name}_{now}_dr.txt')
+    os.system(f'python3 energest-stats.py {results_dir}/test.log >> results/{test_name}_{now}_dc.txt')
